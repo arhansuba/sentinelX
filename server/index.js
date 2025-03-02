5872868fea5f6a4514df85c1108c692c0861f78b
Cipher Zero Protocol <cipherzeroprotocol@gmail.com>
2025-03-02 06:51:51 +0300

    cmt

---
+++ server/index.js
@@ -0,0 +1,11 @@
+
+
+
+
+
+
+
+
+
+
+// ...existing code...app.use(express.urlencoded({ extended: true, limit: '1mb' }));app.use(express.json({ limit: '1mb' }));// Increase header size limit// ...existing code...const app = express();const express = require('express');
